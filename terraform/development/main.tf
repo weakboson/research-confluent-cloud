282d41e16b5411be617c3677a0751e676699a0a9
weakboson <weakboson@gmail.com>
2024-05-04 16:10:34 +0900

    Kafka Cluster 作成までのコード

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -7,3 +7,35 @@ terraform {
     }
   }
 }
+
+# Create Environments
+# https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_environment
+resource "confluent_environment" "development" {
+  display_name = "Development"
+
+  stream_governance {
+    package = "ESSENTIALS"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Create a Kafka Cluster
+# https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
+resource "confluent_kafka_cluster" "development" {
+  display_name = "development-cluster"
+  availability = "SINGLE_ZONE"
+  cloud        = "GCP"
+  region       = "asia-northeast1"
+  basic {}
+
+  environment {
+    id = confluent_environment.development.id
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
